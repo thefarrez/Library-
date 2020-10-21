a8bb1080065ea99212a10ca4c4df1f0d0c0e4fae
Farhad Rezai <faradrezai@gmail.com>
2020-10-21 18:09:06 +0200

    first commit

---
+++ Library/LibraryForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Library {
+    partial class LibraryForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbBooks = new System.Windows.Forms.ListBox();
+            this.BTNChangeBook = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbBooks
+            // 
+            this.lbBooks.FormattingEnabled = true;
+            this.lbBooks.ItemHeight = 20;
+            this.lbBooks.Location = new System.Drawing.Point(18, 18);
+            this.lbBooks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lbBooks.Name = "lbBooks";
+            this.lbBooks.Size = new System.Drawing.Size(271, 364);
+            this.lbBooks.TabIndex = 0;
+            // 
+            // BTNChangeBook
+            // 
+            this.BTNChangeBook.Location = new System.Drawing.Point(310, 18);
+            this.BTNChangeBook.Name = "BTNChangeBook";
+            this.BTNChangeBook.Size = new System.Drawing.Size(122, 54);
+            this.BTNChangeBook.TabIndex = 1;
+            this.BTNChangeBook.Text = "TEST: Change the book title";
+            this.BTNChangeBook.UseVisualStyleBackColor = true;
+            this.BTNChangeBook.Click += new System.EventHandler(this.BTNChangeBook_Click);
+            // 
+            // LibraryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 403);
+            this.Controls.Add(this.BTNChangeBook);
+            this.Controls.Add(this.lbBooks);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "LibraryForm";
+            this.Text = "Library";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbBooks;
+        private System.Windows.Forms.Button BTNChangeBook;
+    }
+}
+
---
+++ Library/Services/BookService.cs
@@ -0,0 +1,47 @@
+﻿using Library.Models;
+using Library.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Services
+{
+    class BookService
+    {
+        /// <summary>
+        /// service doesn't need a context but it needs a repository.
+        /// </summary>
+        BookRepository bookRepository;
+
+        /// <param name="rFactory">A repository factory, so the service can create its own repository.</param>
+        public BookService(RepositoryFactory rFactory)
+        {
+            this.bookRepository = rFactory.CreateBookRepository();
+        }
+
+        public IEnumerable<Book> All()
+        {
+            return bookRepository.All();
+        }
+
+
+        public IEnumerable<Book> GetAllThatContainsInTitle(string a)
+        {
+            return from b in bookRepository.All()
+            where b.Title.Contains(a)
+            select b;
+        }
+
+        /// <summary>
+        /// The Edit method makes sure that the given Book object is saved to the database and raises the Updated() event.
+        /// </summary>
+        /// <param name="b"></param>
+        public void Edit(Book b)
+        {
+            bookRepository.Edit(b);
+            // TODO: Raise the Updated event.
+        }
+    }
+}
